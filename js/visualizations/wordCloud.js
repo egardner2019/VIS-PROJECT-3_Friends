9ae811102e9132722b75c8f497fbc832129213a7
unknown <emilygb88@gmail.com>
2024-04-17 18:12:38 -0400

    Word cloud initial implementation

--- js/visualizations/wordCloud.js
+++ js/visualizations/wordCloud.js
@@ -7,6 +7,9 @@ class WordCloud {
     // TODO: add more configurations (width, height, margins, etc.)
     this.config = {
       parentElement: "#wordcloud",
+      containerWidth: 550,
+      containerHeight: 400,
+      margin: { top: 20, bottom: 50, right: 30, left: 55 },
     };
 
     this.initVis();
@@ -14,6 +17,26 @@ class WordCloud {
 
   initVis() {
     const vis = this;
+    vis.svg = d3
+        .select(vis.config.parentElement)
+        .append("svg")
+        .attr(
+          "width",
+          vis.config.containerWidth +
+            vis.config.margin.left +
+            vis.config.margin.right
+        )
+        .attr(
+          "height",
+          vis.config.containerHeight +
+            vis.config.margin.top +
+            vis.config.margin.bottom
+        )
+        .append("g")
+        .attr(
+          "transform",
+          `translate(${vis.config.margin.left},${vis.config.margin.top})`
+        );
 
     // TODO: add the logic to create the visualization's elements that won't update at all (not dependent upon data)
 
@@ -24,6 +47,34 @@ class WordCloud {
     const vis = this;
     vis.data = wordcloudData;
 
+    vis.svg.selectAll("*").remove();
+
+    const maxCount = d3.max(wordcloudData, d => d.count);
+
+    const layout = d3.layout
+      .cloud()
+      .size([vis.config.containerWidth, vis.config.containerHeight])
+      .words(wordcloudData.map(d => ({ text: d.word, size: d.count })))
+      .padding(5)
+      .rotate(0)
+      .fontSize(d => Math.sqrt(d.size / maxCount) * 70) 
+      .on("end", draw);
+
+    layout.start();
+
+    function draw(words) {
+      vis.svg
+        .append("g")
+        .attr("transform", `translate(${vis.config.containerWidth / 3},${vis.config.containerHeight / 2})`)
+        .selectAll("text")
+        .data(words)
+        .join("text")
+        .style("font-size", d => d.size + "px")
+        .style("fill", "black")
+        .attr("text-anchor", "middle")
+        .attr("transform", d => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
+        .text(d => d.text);
+    }
     // NOTE FOR EMILY B: use the vis.data variable. If it's not in a good format, feel free to modify the vis.data variable
     // ... but I would shy away from modifying the wordcloudData variable without contacting Emma
     // If there are words in the data that you don't want to include, simply add the word you want to hide to the
